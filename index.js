08798d780d7526ac1dc1425e030ba8b109f33184
mkrof <mkrof@users.noreply.github.com>
2017-01-12 15:21:26 -0500

    Implement scaffolding.

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const inquirer = require('inquirer');
 const path = require('path');
+const inquirer = require('inquirer');
+const nunjucks = require('nunjucks');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs-extra'));
 
-const questions = [{
-  name: 'projectName',
-  message: 'Project name: ',
-  default: process.cwd().split(path.sep).pop()
-}];
+const config = {
+  paths: {
+    templateDirectory: `${__dirname}/tpl`
+  },
+  questions: [{
+    name: 'projectName',
+    message: 'Project name: ',
+    default: process.cwd().split(path.sep).pop()
+  }]
+};
 
-inquirer.prompt(questions).then(scaffold);
+nunjucks.configure(config.paths.templateDirectory);
+
+inquirer.prompt(config.questions)
+  .then(scaffold)
+  .then(() => console.log('Finished.'), err => console.log(`✖ ${err}`));
 
 function scaffold (answers) {
-  console.log(answers);
+  return Promise.all([
+    copyFile('.babelrc'),
+    renderFile('package.json', answers)
+  ])
+}
+
+function renderFile (filename, data) {
+  return fs.writeFileAsync(filename, nunjucks.render(`${filename}`, data))
+    .then(() => console.log(`✓ Generated ${filename}`));
+}
+
+function copyFile (filename) {
+  return fs.copyAsync(`${config.paths.templateDirectory}/${filename}`, filename)
+    .then(() => console.log(`✓ Copied ${filename}`))
 }
